8e523b1d022125537de55ade3a81526d3ad217a4
蕭晴方 <sunny128@xiaoqingfangdemacbook-air.local>
2016-10-05 19:35:09 +0800

    Initial Commit

---
+++ sunny app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sunny app
+//
+//  Created by 蕭晴方 on 2016/10/5.
+//  Copyright © 2016年 蕭晴方. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
